59abb4fe2d70284e2d76d3045ba93fa141feb37d
Tomas Kvapil <tkvapi2@uic.edu>
2019-06-12 15:34:08 -0500

    Completed the module

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 const app = express();
 
-const { accounts, users, writeJSON } = require('/data');
+const { accounts, users, writeJSON } = require('./data');
 
 //configure directory to where our views can be found
 app.set('views', path.join(__dirname, 'views'));

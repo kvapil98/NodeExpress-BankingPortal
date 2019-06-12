c6528e988e504057039566a0b9b8246d5b21c45f
Tomas Kvapil <tkvapi2@uic.edu>
2019-06-12 12:15:12 -0500

    Completed the module

--- src/app.js
+++ src/app.js
@@ -12,6 +12,33 @@ app.set('view engine', 'ejs');
 //All of our CSS/JS for the client-side is found in the public directory. We need to point express to public
 app.use(express.static(path.join(__dirname, 'public'))); 
 
-app.get('/', (req,res) => res.render('index', {title:'Index'}));
+//read json file and convert to a javascript object
+const accountData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'),'utf8'
+);
+const accounts = JSON.parse(accountData);
+
+//read json file and convert to a javascript object
+const userData = fs.readFileSync(
+    path.join(__dirname, 'json', 'users.json'), 'utf8'
+);
+const users = JSON.parse(userData);
+
+//index route, acounts route
+app.get('/', (req,res) => res.render('index', {title:'Account Summary', accounts}));
+
+app.get('/savings', (req,res) => {
+    res.render('account', {account: accounts.savings});
+});
+app.get('/checking', (req,res) => {
+    res.render('account', {account: accounts.checking});
+});
+app.get('/credit', (req,res) => {
+    res.render('account', {account: accounts.credit});
+});
+
+app.get('/profile', (req,res) => {
+    res.render('profile', {user: users[0]});
+});
 
 app.listen(3000, () => console.log("PS Project Running on port 3000!"));
